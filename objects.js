c14c5894be5c44452699f687f0b0e6882c97056a
LeezaLuncheon Luncheon <leezaluncheon@gmail.com>
2018-03-06 05:57:13 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = new Object({'Donna Summer':'Grand Illusion','Chic':'Everybody Dance'})
+function updatePlaylist(obj,artistName,songTitle){
+  //obj[artistName] = songTitle
+  return Object.assign({}, obj, artistName:songTitle)
+
+}
+
+function removeFromPlaylist(obj,artistName){
+  delete obj.artistName
+  return obj
+}
